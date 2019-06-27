b9ffe51833fdd84aef7396cacf9f53f30c528e4c
DH <davidhoogstraaten@gmail.com>
2019-06-27 10:51:00 +0000

    master

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const PORT =process.env.PORT || 8080;
 let router = express.Router();
 
 router.get('/',function(req,res){
- res.sendFile(__dirname + '/daw/index.html');
+ res.sendFile(__dirname + '../daw/index.html');
 });
-app.use(express.static(__dirname + '/daw'));
+app.use(express.static(__dirname + '../daw'));
+
 app.use('/',router);
 
 app.listen(PORT,function(){
