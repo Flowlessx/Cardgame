bd46f44c99ae12cfe8dc3ebfac0af1a0334901c5
DH <davidhoogstraaten@gmail.com>
2019-06-28 18:06:14 +0000

    reset

--- daw/src/ui/effects.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function UIeffectsInit() {
-  /* empty */
-}
-
-function UIeffectsSelectChan( _id ) {
-	// lg(DAW.get.channel( id ).name)
-}
--- daw/src/ui/title.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function UItitle() {
-	const name = DAW.get.name() || "GridSound";
-
-	document.title = DAW.compositionNeedSave() ? `*${ name }` : name;
-}
